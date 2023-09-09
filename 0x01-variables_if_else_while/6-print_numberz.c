4dc4ed5e381e6ba613f96a89adb1cdc1cf00e1ba
jana <ganaassem2122003@gmail.com>
2023-09-08 17:33:58 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ * Description: prints all single digit numbers of base 10
+ * Return: always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
