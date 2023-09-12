3c387a31f609f1d8bc94eae9bf756d03960a2bd6
jana <ganaassem2122003@gmail.com>
2023-09-12 15:03:48 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	{
+		putchar(str[ch]);
+	}
+
+	putchar("\n");
+	return (0);
+
+
+}
