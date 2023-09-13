547301c6d9d17cc41dd1a6c8a50ea04343d2535b
jana <ganaassem2122003@gmail.com>
2023-09-12 19:05:08 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar((i * j) + '0');
+			_putchar(44)
+			_putchar(32)
+		}
+		_putchar('\n')	
+	}
+}
