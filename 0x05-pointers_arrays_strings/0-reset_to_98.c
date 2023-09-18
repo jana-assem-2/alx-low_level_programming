42ee9d19625c4092e21e38cf865ebd14da3a88f6
jana <ganaassem2122003@gmail.com>
2023-09-18 16:50:06 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
- /**
-  * reset_to_98 - reset argument to 98
-  * @n: printed integer
-  * Return: void
-  */
 
-  void reset_to_98(int *n)
-  {
+/**
+* reset_to_98 - reset argument to 98
+* @n: printed integer
+* Return: void
+*/
 
-    *n = 98;
-
- }
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
