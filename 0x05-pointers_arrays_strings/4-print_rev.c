41ab9d0c88422ef0c8b6b03ac8d5dc6fa86e0c8e
jana <ganaassem2122003@gmail.com>
2023-09-18 17:28:41 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _print_rev - print reversed string, followed by a new line
+* print_rev - print reversed string, followed by a new line
 * @s: pointer to the string to point
 * Return: void
 */
 
-void _print_rev(char *s)
+void print_rev(char *s)
 {
 	int i = 0;
 
