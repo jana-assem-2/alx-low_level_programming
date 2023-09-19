056445041c0d0f2c5334e256413600ee2a90521c
jana <ganaassem2122003@gmail.com>
2023-09-18 18:39:30 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_array - print the element of an array
+* @a: name of an array
+* @n: size of an array
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	_putchar('\n');
+}
