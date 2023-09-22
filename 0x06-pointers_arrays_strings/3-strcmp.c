4cd0ad9296eaf89cc8a4d98eae03f653f12345c5
jana <ganaassem2122003@gmail.com>
2023-09-22 16:35:48 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] == '\0'; i++)
+	while (*s1)
 	{
-		if (s1[i] < s2[i])
-			return (-15);
-		else if (s1[i] > s2[i])
-			return (15);
-		else
-			return (0);
-
+		if (*s1 != *s2)
+		{
+			break;
+		}
+		s1++;
+		s2++;
 	}
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
