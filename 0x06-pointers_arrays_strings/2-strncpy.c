b9f35e6a2bf8f064373484269f301973733cc4eb
jana <ganaassem2122003@gmail.com>
2023-09-22 15:29:35 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - this is startpoint
+ * this is a program that do a  function that copies a string.
+ * Return: dest
+ * @dest:this is the var that return with the copy content
+ * @src: this is the var that i copy from
+ * @n: this is the number that  i copy the content and stop to the index n
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (i < n - 1);
+	return (dest);
+}
