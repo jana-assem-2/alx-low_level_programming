3bd89d3e8c6d9e9bd9f92099a857dec56674f3dd
jana <ganaassem2122003@gmail.com>
2023-09-28 14:34:54 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(s[i]);
+		_putchar(*s);
+		s++;
+		_puts_recursion(*s);
 
 	}
 	else
