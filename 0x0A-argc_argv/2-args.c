5fce4e90ebe46d1af4738d02da645393da4ef045
jana <ganaassem2122003@gmail.com>
2023-10-02 18:06:48 -0700

    argc and argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,11 +11,13 @@
 
 int main(int argc, char *argv[])
 {
+	int i = 0;
+
 	(void)argc;
-	while (--argc)
+	while (i < argc)
 	{
-		printf("%s\n", argv[argc]);
+		printf("%s\n", argv[i]);
+		i++;
 	}
 	return (0);
 }
-
