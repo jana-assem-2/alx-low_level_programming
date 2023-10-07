c4d0b1548a7924bcf7e5c03eb0f5b60d5969e742
jana <ganaassem2122003@gmail.com>
2023-10-07 09:54:14 -0700

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,6 +15,8 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	ptr = (int *) malloc(len * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < len; i++)
 		ptr[i] = min + i;
 	return (ptr);
